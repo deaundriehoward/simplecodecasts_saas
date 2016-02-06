51757be7864a9b5ac00ce88ee908784195633c79
deaundriehoward <deaundrie7@gmail.com>
2016-02-06 17:03:18 +0000

    Edited ActionMailer/SendGrid

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,12 @@
 class ContactMailer < ActionMailer::Base
- default to: "deaundrie7@gmail.com
- "
-    def contact_mail(name, email, body)
+ default to: 'deaundrie7@gmail.com'
+ 
+    def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
         
-        mail(from: email, subject: "Contact From ")
+        mail(from: email, subject: 'Contact Form Message')
     end
     
 end
